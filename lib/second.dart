471cab533d938b27d42413eda15ba3d18a581070
Roshan-Barkane <roshanbaekhane987@gmail.com>
2023-08-16 22:21:38 +0530

    GradientColors

--- lib/second.dart
+++ lib/second.dart
@@ -11,36 +11,74 @@ class _secondState extends State<second> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Color.fromRGBO(27, 12, 230, 0.965),
-        title: const Center(
-          child: Text(
-            ' Happy Independence Day ',
-            style: TextStyle(color: Colors.white, fontSize: 30),
+        appBar: AppBar(
+          backgroundColor: const Color.fromRGBO(27, 12, 230, 0.965),
+          title: const Center(
+            child: Text(
+              ' Column Widght ',
+              style: TextStyle(color: Colors.white, fontSize: 30),
+            ),
           ),
         ),
-      ),
-      body: Center(
-        child: Container(
-          width: 250,
-          height: 300,
-          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-          decoration: const BoxDecoration(
-              color: Colors.orange,
-              boxShadow: [BoxShadow(blurRadius: 20.0)],
-              gradient: SweepGradient(
-                colors: [
-                  Color.fromARGB(255, 224, 15, 85),
-                  Color.fromARGB(255, 248, 148, 148)
-                ],
-              )),
-          child: const Text(
-            'Container Weights',
-            style: TextStyle(
-                fontSize: 30, color: Color.fromARGB(255, 255, 255, 255)),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              Container(
+                  padding: const EdgeInsets.all(20),
+                  decoration: const BoxDecoration(
+                      gradient:
+                          LinearGradient(colors: [Colors.red, Colors.yellow])),
+                  child: const Text(
+                    'Colunm Widget 1',
+                    style: TextStyle(fontSize: 30.0),
+                  )),
+              Container(
+                  padding: const EdgeInsets.all(20),
+                  decoration: const BoxDecoration(
+                      gradient: LinearGradient(colors: [
+                    Color.fromARGB(255, 109, 230, 10),
+                    Color.fromARGB(255, 27, 56, 223)
+                  ])),
+                  child: const Text(
+                    'Colunm Widget 2',
+                    style: TextStyle(fontSize: 30.0, color: Colors.white),
+                  )),
+              Container(
+                  padding: const EdgeInsets.all(20),
+                  decoration: const BoxDecoration(
+                      gradient: LinearGradient(colors: [
+                    Color.fromARGB(255, 243, 4, 171),
+                    Color.fromARGB(255, 201, 83, 93)
+                  ])),
+                  child: const Text(
+                    'Colunm Widget 3',
+                    style: TextStyle(fontSize: 30.0),
+                  )),
+              Container(
+                  padding: const EdgeInsets.all(20),
+                  decoration: const BoxDecoration(
+                      gradient: LinearGradient(colors: [
+                    Color.fromARGB(255, 255, 99, 37),
+                    Color.fromARGB(255, 4, 248, 16)
+                  ])),
+                  child: const Text(
+                    'Colunm Widget 4',
+                    style: TextStyle(fontSize: 30.0, color: Colors.white),
+                  )),
+              Container(
+                  padding: const EdgeInsets.all(20),
+                  decoration: const BoxDecoration(
+                      gradient: LinearGradient(colors: [
+                    Color.fromARGB(255, 21, 243, 187),
+                    Color.fromARGB(255, 0, 204, 255)
+                  ])),
+                  child: const Text(
+                    'Colunm Widget 5',
+                    style: TextStyle(fontSize: 30.0),
+                  ))
+            ],
           ),
-        ),
-      ),
-    );
+        ));
   }
 }
